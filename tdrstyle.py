23dff83f8bb0df4e8952f384a8895227d563bb2c
Andrea Malara <andrea.malara@cern.ch>
2023-09-27 11:31:23 +0200

    fix overlay of axes

--- tdrstyle.py
+++ tdrstyle.py
@@ -198,7 +198,7 @@ def setTDRStyle():
     # For the axis labels:
     tdrStyle.SetLabelColor(1, 'XYZ')
     tdrStyle.SetLabelFont(42, 'XYZ')
-    tdrStyle.SetLabelOffset(0.007, 'XYZ')
+    tdrStyle.SetLabelOffset(0.01, 'XYZ')
     tdrStyle.SetLabelSize(0.05, 'XYZ')
     # For the axis:
     tdrStyle.SetAxisColor(1, 'XYZ')
