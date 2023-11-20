c319e27e10505973c4f36d8d52d14054cb4159e0
Tommaso Tedeschi <tedesk95@gmail.com>
2023-11-20 17:21:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,6 @@ setup(
     install_requires=[],
 
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 )
