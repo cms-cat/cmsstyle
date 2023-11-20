b3def98790fcd5daec4e0d31971eca3f1401698c
root <tedesk95@gmail.com>
2023-11-20 15:55:05 +0000

    modify to make this pip installable

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='tdrstyle',
+    version='0.1.0',    
+    description='CMS plotting style ROOT macros',
+    url='https://github.com/cms-cat/cms-root-style',
+    author='CMS Common Analysis Tools',
+    #author_email='',
+    license='BSD 2-clause',
+    packages=['tdrstyle'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
---
+++ tdrstyle/__init__.py
@@ -0,0 +1,3 @@
+from .tdrstyle import *
+
+__version__ = "0.1.0"
