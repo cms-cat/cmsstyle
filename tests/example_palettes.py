d0e33a98937c3e116ed53d91af28530803a0cd69
Tommaso Tedeschi <tedesk95@gmail.com>
2024-01-26 16:44:34 +0100

    Update and rename example_stack.py to example_palettes.py

--- tests/example_palettes.py
+++ tests/example_palettes.py
@@ -20,6 +20,13 @@ class Plotter:
                 #h.Scale(1.0 / h.Integral())
             self.bkgs.append(h)
 
+        self.hist2d = ROOT.TH2F("hist2d", "2D Histogram", 25, 0, 5, 25, 0, 5)
+        for i in range(200000):
+            x = np.random.normal(2.5, 1)
+            y = np.random.normal(2.5, 1)
+            self.hist2d.Fill(x, y)
+        self.hist2d.Scale(10.0 / self.hist2d.Integral())
+
     def Plot(self, square, iPos):
         canv_name = f'example_{"square" if square else "rectangle"}_pos{iPos}'
         CMS.SetLumi("")
@@ -56,13 +63,44 @@ class Plotter:
         # Takes care of fixing overlay and closing object
         CMS.SaveCanvas(canv, os.path.join(self.outputPath, canv_name + ".pdf"))
 
+    def Plot2D(self, square, iPos):
+        canv_name = f'example_2D_{"square" if square else "rectangle"}_pos{iPos}'
+        # Allow to reduce the size of the lumi info
+        scaleLumi = 0.80 if square else None
+        canv = CMS.cmsCanvas(
+            canv_name,
+            0,
+            5,
+            0,
+            5,
+            "X",
+            "Y",
+            square=square,
+            extraSpace=0.01,
+            iPos=iPos,
+            with_z_axis=True,
+            scaleLumi=scaleLumi,
+        )
+
+        self.hist2d.GetZaxis().SetTitle("Events normalised")
+        self.hist2d.GetZaxis().SetTitleOffset(1.4 if square else 1.2)
+        self.hist2d.Draw("same colz")
+        # Set a new palette
+        CMS.SetPalette()
+
+        # Allow to adjust palette position
+        CMS.UpdatePalettePosition(self.hist2d, canv)
+
+        CMS.SaveCanvas(canv, os.path.join(self.outputPath, canv_name + ".pdf"))
+
 def main():
     plotter = Plotter()
     plotter.Plot(square=CMS.kSquare, iPos=0)
     plotter.Plot(square=CMS.kRectangular, iPos=0)
     plotter.Plot(square=CMS.kSquare, iPos=11)
     plotter.Plot(square=CMS.kRectangular, iPos=11)
-
+    plotter.Plot2D(square=CMS.kSquare, iPos=0)
+    plotter.Plot2D(square=CMS.kRectangular, iPos=0)
 
 if __name__ == "__main__":
     main()
